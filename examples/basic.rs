a1694e457c44b4214bb82b28de6ee85c8c9f8af6
Johan Andersson <repi@repi.se>
2020-02-03 23:17:34 +0100

    Extend example

--- examples/basic.rs
+++ examples/basic.rs
@@ -8,15 +8,32 @@ fn heavy_calc1() -> Wrapping<u64> {
     v
 }
 
+fn heavy_calc2() -> Wrapping<u64> {
+    let mut v = Wrapping(1u64);
+    for i in 0..800_000_000 {
+        v += v * Wrapping(i);
+    }
+    v
+}
+
+fn big_fn() {
+    // work 1
+
+    superluminal_perf::begin_event(b"example-work1\0");
+    let v = heavy_calc1();
+    superluminal_perf::end_event();
+    println!("work1 done: {}", v);
+
+    // work 2
+
+    superluminal_perf::begin_event(b"example-work2\0");
+    let v = heavy_calc2();
+    superluminal_perf::end_event();
+    println!("work2 done: {}", v);
+}
+
 fn main() {
     println!("Enabled: {}", superluminal_perf::enabled());
 
-    // work 1
-
-    superluminal_perf::begin_event(b"example_loop_1\0");
-    let v = heavy_calc1();
-    superluminal_perf::end_event();
-    println!("work done: {}", v);
-
-    // work 2
+    big_fn();
 }
