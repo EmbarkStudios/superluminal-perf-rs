8c37f2fb9130018cc3c3fb80a55c2b40c8a8c806
Johan Andersson <repi@repi.se>
2020-02-03 20:40:22 +0100

    Fix example

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,11 +1,12 @@
 use std::num::Wrapping;
 use std::time::Instant;
 
-fn something_heavy1() {
+fn heavy_calc1() -> Wrapping<u64> {
     let mut v = Wrapping(1u64);
     for i in 0..1_000_000_000 {
         v += v * Wrapping(i);
     }
+    v
 }
 
 fn main() {
@@ -13,9 +14,9 @@ fn main() {
 
     let start_time = Instant::now();
     superluminal_perf::begin_event(b"example_loop_1\0");
-    something_heavy1();
+    let v = heavy_calc1();
     superluminal_perf::end_event();
-    println!("work 1 done [{} ms]", start_time.elapsed().as_millis());
+    println!("work done: {} [{} ms]", v, start_time.elapsed().as_millis());
 
     // work 2
 }
