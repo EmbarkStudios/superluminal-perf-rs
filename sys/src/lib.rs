2f539f5cab98041e99469d596fe341708328890e
Ritesh Oedayrajsingh Varma <rovarma@gmail.com>
2020-02-11 19:35:08 +0100

    Fixed crash when using Release version of PerformanceAPI
    The return value of PerformanceAPI_EndEvent in the FFI interface was declared incorrectly (i.e. missing). This caused Rust and C to disagree about how much stack space should be reserved prior to calls to PerformanceAPI_EndEvent, which then caused a crash in PerformanceAPI_EndEvent when it tries to write the return value to the stack. Fixed by declaring the return value correctly.
    
    Note that the return value is unused as it is only there to prevent calls to PerformanceAPI_EndEvent from optimized out due to tail call elimination; its contents are irrelevant. To prevent Rust users from having to deal with it, the `end_event` function internally ignores the return value.

--- sys/src/lib.rs
+++ sys/src/lib.rs
@@ -2,10 +2,16 @@ use libc::c_char;
 
 pub const DEFAULT_COLOR: u32 = 0xFF_FF_FF_FF;
 
+#[repr(C)]
+pub struct PerformanceAPI_SuppressTailCallOptimization 
+{ 
+    _private: [u64; 3] 
+}
+
 extern "C" {
     pub fn PerformanceAPI_SetCurrentThreadName(in_thread_name: *const c_char);
 
     pub fn PerformanceAPI_BeginEvent(in_id: *const c_char, in_data: *const c_char, in_color: u32);
 
-    pub fn PerformanceAPI_EndEvent();
+    pub fn PerformanceAPI_EndEvent() -> PerformanceAPI_SuppressTailCallOptimization;
 }
