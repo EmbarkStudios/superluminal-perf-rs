1484fd642597cf4715b53d1a025b8a5e8f306a81
Johan Andersson <repi@repi.se>
2021-03-11 11:19:52 +0100

    Use Embark standard lints v0.3
    Fix #13

--- src/lib.rs
+++ src/lib.rs
@@ -27,30 +27,61 @@
 //!
 //! - `enable` - this flag is used by default and enables calling the Superluminal Performance API. This can be useful to only enable the events only for specific application features
 
-#![allow(unused_variables)]
+// BEGIN - Embark standard lints v0.3
+// do not change or add/remove here, but one can add exceptions after this section
+// for more info see: <https://github.com/EmbarkStudios/rust-ecosystem/issues/59>
+#![deny(unsafe_code)]
 #![warn(
     clippy::all,
-    clippy::doc_markdown,
+    clippy::await_holding_lock,
     clippy::dbg_macro,
-    clippy::todo,
+    clippy::debug_assert_with_mut_call,
+    clippy::doc_markdown,
     clippy::empty_enum,
     clippy::enum_glob_use,
-    clippy::pub_enum_variant_names,
-    clippy::mem_forget,
-    clippy::use_self,
+    clippy::exit,
+    clippy::explicit_into_iter_loop,
     clippy::filter_map_next,
-    clippy::needless_continue,
+    clippy::fn_params_excessive_bools,
+    clippy::if_let_mutex,
+    clippy::imprecise_flops,
+    clippy::inefficient_to_string,
+    clippy::large_types_passed_by_value,
+    clippy::let_unit_value,
+    clippy::linkedlist,
+    clippy::lossy_float_literal,
+    clippy::macro_use_imports,
+    clippy::map_err_ignore,
+    clippy::map_flatten,
+    clippy::map_unwrap_or,
+    clippy::match_on_vec_items,
+    clippy::match_same_arms,
+    clippy::match_wildcard_for_single_variants,
+    clippy::mem_forget,
+    clippy::mismatched_target_os,
     clippy::needless_borrow,
-    rust_2018_idioms,
+    clippy::needless_continue,
+    clippy::option_option,
+    clippy::pub_enum_variant_names,
+    clippy::ref_option_ref,
+    clippy::rest_pat_in_fully_bound_structs,
+    clippy::string_add_assign,
+    clippy::string_add,
+    clippy::string_to_string,
+    clippy::suboptimal_flops,
+    clippy::todo,
+    clippy::unimplemented,
+    clippy::unnested_or_patterns,
+    clippy::unused_self,
+    clippy::verbose_file_reads,
     future_incompatible,
-    trivial_numeric_casts,
-    unstable_features,
     nonstandard_style,
-    unused_import_braces,
-    unused_qualifications,
-    unused_results,
-    missing_docs
+    rust_2018_idioms
 )]
+// END - Embark standard lints v0.3
+// crate-specific exceptions:
+#![allow(unused_variables)]
+#![allow(unsafe_code)]
 
 #[cfg(test)]
 mod test;
