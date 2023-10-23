de801dd2ad6d0913073b568b73a4e862060f4fce
Paul Mabileau <paul.mabileau@harfanglab.fr>
2023-10-23 12:59:35 +0200

    Add support for x86 linking
    Signed-off-by: Paul Mabileau <paul.mabileau@harfanglab.fr>

--- sys/build.rs
+++ sys/build.rs
@@ -1,5 +1,7 @@
+use std::env;
+
 fn runtime() -> &'static str {
-    if std::env::var("CARGO_CFG_TARGET_FEATURE")
+    if env::var("CARGO_CFG_TARGET_FEATURE")
         .unwrap_or_default()
         .contains("crt-static")
     {
@@ -9,10 +11,19 @@ fn runtime() -> &'static str {
     }
 }
 
+fn arch() -> &'static str {
+    if env::var("CARGO_CFG_TARGET_ARCH").as_deref() == Ok("x86") {
+        "x86"
+    } else {
+        "x64"
+    }
+}
+
 fn main() {
     println!(
-        "cargo:rustc-link-search={}/external/lib/x64/",
-        std::env!("CARGO_MANIFEST_DIR")
+        "cargo:rustc-link-search={}/external/lib/{}/",
+        std::env!("CARGO_MANIFEST_DIR"),
+        arch(),
     );
     println!("cargo:rustc-link-lib=PerformanceAPI_{}", runtime());
 }
